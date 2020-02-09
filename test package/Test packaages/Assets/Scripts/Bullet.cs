7a662f176dcd52969febac9cc03e465b8c33d9d9
simon13456 <simonlerou@gmail.com>
2020-02-09 16:49:45 -0500

    fat fat fat

---
+++ test package/Test packaages/Assets/Scripts/Bullet.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour {
+
+    private Transform _target = null;
+    [SerializeField] private float _speed = 30f;
+    [SerializeField] private GameObject _gunShotFX;
+    private float bulletSpeed;
+    private Vector3 _direction;
+
+    public void Seek (Transform target) {
+        _target = target;
+    }
+
+    public void Start() {
+        _direction = _target.position - transform.position;
+        Instantiate(_gunShotFX, transform.position, transform.rotation);
+        //Faire du son aussi
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (_target==null) {
+            Destroy(gameObject);
+            return;
+        }
+        bulletSpeed = _speed * Time.deltaTime;
+        transform.Translate(_direction.normalized * bulletSpeed, Space.World);
+
+        /*
+        if (Bullet frappe le fons ou dequoi dautre) {
+            Destroy(gameObject);
+        }*/
+    }
+}
---
+++ test package/Test packaages/Assets/Scripts/Enemy.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+
+    [Header("GameObjects")]
+    [SerializeField] private Transform _target;
+    [SerializeField] private Transform _playerHead;
+    [SerializeField] private Transform _head;
+    [SerializeField] private Transform _partToRotate;
+
+    [SerializeField] private Animator _anim;
+    private bool isVisible = true;
+
+    [Header("Tir")]
+    [SerializeField] private GameObject _bulletPrefab;
+    [SerializeField] private Transform _firePoint;
+    [SerializeField] private float _fireRate = 1f;
+    private float _fireCountDown = 0f;
+
+    // Start is called before the first frame update
+    void Start() {
+        _anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (isVisible && _fireCountDown<=0f) {
+            //_anim.SetBool("isFiring", true);
+            Shoot();
+            _fireCountDown = 1f / _fireRate;
+        }
+        else {
+            //_anim.SetBool("isFiring", false);
+        }
+        _fireCountDown -= Time.deltaTime;
+
+        Vector3 direction = _playerHead.position - _head.position;
+        _partToRotate.rotation = Quaternion.LookRotation(direction);
+        _partToRotate.rotation *= Quaternion.Euler(0f, 90f, -90f);
+    }
+
+    private void Shoot() {
+        GameObject bulletGO = (GameObject)Instantiate(_bulletPrefab, _firePoint.position, _firePoint.rotation);
+        Bullet bullet = bulletGO.GetComponent<Bullet>();
+        if (bullet != null) {
+            bullet.Seek(_target);
+        }
+
+    }
+}
